58fbf13caec12ee9fccd5c0bc8d985e088622a54
Ashu1234 <gidaveerashwini@gmail.com>
2024-12-26 23:31:31 +0530

    props concept

---
+++ learnstate/src/App.js
@@ -0,0 +1,20 @@
+import React , { useState } from "react";
+// import logo from './assets/logo.svg'; 
+// import {useState} from 'react';
+
+export default function App() {
+  // let name = '';
+  const {name , setName} = useState('Neha')
+
+  const handleclick = ()=> {
+    // name = 'Akash';
+    // alert(name);
+    setName('Akash');
+  }
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <button onClick={handleclick}>Click</button>
+    </div>
+  )
+}
